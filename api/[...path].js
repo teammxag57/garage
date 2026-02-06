9ad16aa2a4c625f843460af5369afb2fa2ae9c85
Caldeira Caldeira&Caldeira Caldeira <geral@caldeiraecaldeira.pt>
2026-02-06 11:52:03 +0000

    Use Vercel serverless catch-all for Express API

--- api/[...path].js
+++ api/[...path].js
@@ -2,13 +2,14 @@ import express from "express";
 import garagemRouter from "../routes/garagem.js";
 
 const app = express();
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Opcional: health check
+app.get("/", (req, res) => res.status(200).send("API online"));
+
 app.use("/garagem", garagemRouter);
 
-// Catch-all handler para o Vercel
 export default function handler(req, res) {
   return app(req, res);
 }
