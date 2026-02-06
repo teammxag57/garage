bbb7bd674bd3bb9ff4d319555a80721b093c246b
Caldeira Caldeira&Caldeira Caldeira <geral@caldeiraecaldeira.pt>
2026-02-06 12:16:43 +0000

    fix vercel routing

--- api/index.js
+++ api/index.js
@@ -2,12 +2,18 @@ import express from "express";
 import garagemRouter from "../routes/garagem.js";
 
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Rotas
 app.get("/", (req, res) => res.status(200).send("API online"));
 app.use("/garagem", garagemRouter);
 
 export default function handler(req, res) {
+  // Remove prefixo /api se existir
+  const path = req.url.replace(/^\/api/, '') || '/';
+  req.url = path;
+  
   return app(req, res);
 }
