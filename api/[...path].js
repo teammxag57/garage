d88a05fc5d033323533daea7d0aea72c9bb2cdde
Caldeira Caldeira&Caldeira Caldeira <geral@caldeiraecaldeira.pt>
2026-02-06 11:57:58 +0000

    Add Vercel catch-all API function

--- api/[...path].js
+++ api/[...path].js
@@ -5,9 +5,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Opcional: health check
 app.get("/", (req, res) => res.status(200).send("API online"));
-
 app.use("/garagem", garagemRouter);
 
 export default function handler(req, res) {
